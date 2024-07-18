51e0c6f0a1281f57b2aa4f73fda6050e36df0984
nadinlutfiah <152040509+nadinlutfiah@users.noreply.github.com>
2024-07-18 11:17:30 +0700

    Add files via upload

---
+++ webmakanan/belajar.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    // Add code to handle delivery address input and button click
+    // Add code to handle food category links
+    // Add code to handle privacy policy link
+});
